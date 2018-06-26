e2f6c08aec638f08aa6e29f6b51edabf21efee87
zhangwei <zhangwei@hugedata.com.cn>
2018-06-26 16:56:07 +0800

    modify maven profile to fix problem that param activationByDefault could change idea resource root

--- ssm-business-dao/src/main/resources/db/migration/V1__User.sql
+++ ssm-business-dao/src/main/resources/db/migration/V1__User.sql
@@ -1,10 +1,20 @@
 /*==============================================================*/
-/* Table: UC_MANAGE_USER_ROLE                                   */
+/* Table: USER                                  */
 /*==============================================================*/
-create table UC_MANAGE_USER_ROLE
-(
-   ID                   INTEGER not null auto_increment,
-   USER_ID              INTEGER not null,
-   ROLE_ID              VARCHAR(32) not null,
-   primary key (ID)
+CREATE TABLE user  (
+  user_id Integer(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  account varchar(255) NOT NULL,
+  nickname varchar(255) NULL DEFAULT NULL,
+  mobile varchar(20) NOT NULL,
+  password varchar(255) NOT NULL,
+  email varchar(255) DEFAULT NULL,
+  gender tinyint(1) NULL DEFAULT 1,
+  city varchar(255)  DEFAULT NULL,
+  birth date NULL DEFAULT NULL,
+  join_time date NOT NULL,
+  locked tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`user_id`) USING BTREE,
+  UNIQUE INDEX `mobile`(`mobile`) USING BTREE,
+  UNIQUE INDEX `account`(`account`) USING BTREE,
+  UNIQUE INDEX `email`(`email`) USING BTREE
 );
