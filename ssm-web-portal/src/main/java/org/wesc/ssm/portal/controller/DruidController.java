22c4f1799daef79e6ea75621e9be15e94af70b2b
Wesley Cheung <zhangwei4815@live.com>
2018-07-20 21:04:41 +0800

    add druid monitor in ssm web application

---
+++ ssm-web-portal/src/main/java/org/wesc/ssm/portal/controller/DruidController.java
@@ -0,0 +1,19 @@
+package org.wesc.ssm.portal.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/druid")
+public class DruidController {
+
+    @RequestMapping("/index")
+    public String index(Model model, HttpServletRequest request) {
+        return "durid/index";
+    }
+
+}
