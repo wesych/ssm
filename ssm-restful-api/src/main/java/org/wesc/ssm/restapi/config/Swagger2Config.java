73f199dca5bdbfb6278b7932c02918047ebfcc8e
zhangwei <zhangwei@hugedata.com.cn>
2018-08-27 15:04:52 +0800

    add swagger2 support to manage api in springmvc project

--- ssm-restful-api/src/main/java/org/wesc/ssm/restapi/config/Swagger2Config.java
+++ ssm-restful-api/src/main/java/org/wesc/ssm/restapi/config/Swagger2Config.java
@@ -1,26 +1,38 @@
-package org.wesc.ssm.portal.config;
+package org.wesc.ssm.restapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
+ * Swagger2配置类.
+ *
  * @Auther: Wesley Cheung
  * @Date: 2018/8/22 10:15
  */
-//@Configuration
-//@EnableSwagger2
+@Configuration
+@EnableWebMvc
+@EnableSwagger2
 public class Swagger2Config {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("SSM框架 API").description("").termsOfServiceUrl("http://localhost:8080").version("1.0").build();
     }
 }
