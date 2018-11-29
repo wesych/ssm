3a7af25726099d397d25ee4f07f3b4a4a63cde90
zhangwei <zhangw985@chinaunicom.cn>
2018-11-29 14:45:38 +0800

    use redis in standalone mode to replace sentinel mode

--- ssm-core-dao/src/main/resources/redis-windows/service-start.bat
+++
@@ -1,9 +0,0 @@
-rem start redis service
-redis-server --service-start --service-name redis6379
-redis-server --service-start --service-name redis6380
-redis-server --service-start --service-name redis6381
-
-rem start sentinel service
-redis-server --service-start --service-name sentinel16379
-redis-server --service-start --service-name sentinel26379
-redis-server --service-start --service-name sentinel36379
--- ssm-core-dao/src/main/resources/redis-windows/service-uninstall.bat
+++
@@ -1,9 +0,0 @@
-rem uninstall redis service
-redis-server --service-uninstall --service-name redis6379
-redis-server --service-uninstall --service-name redis6380
-redis-server --service-uninstall --service-name redis6381
-
-rem uninstall sentinel service
-redis-server --service-uninstall --service-name sentinel16379
-redis-server --service-uninstall --service-name sentinel26379
-redis-server --service-uninstall --service-name sentinel36379
